b9a7d979c04286f0374c91fdafb85ac490dc883e
AdamGitDotNet <adamleon@tlen.pl>
2024-01-03 20:49:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using eventy;
+
+Console.WriteLine("Hello, World!");
+
+
+var filePath = @"C:\temp\events.txt";
+var fileEvent = new FileEventExample(filePath);
+fileEvent.FileChanged += FileEvent_FileChanged;
+
+void FileEvent_FileChanged(string content)
+{
+    Console.WriteLine(content);
+}
+
+Console.ReadLine();
